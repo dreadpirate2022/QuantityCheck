0a6dd87be82884d0c0ac408f5919eac4defc389a
dreadpirate2022 <mirko.cvetanoski@gmail.com>
2022-11-24 11:29:15 +0100

    Switched from sqlite3 to postgresql

--- quantitycheck/settings.py
+++ quantitycheck/settings.py
@@ -124,10 +124,21 @@ WSGI_APPLICATION = 'quantitycheck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'QuantityCheckDatabase',
+        'USER': 'postgres',
+        'PASSWORD': 'cvetanoski',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
